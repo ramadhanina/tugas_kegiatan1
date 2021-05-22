f25cc9f0a67bb9985e28af1b519d2c8e077ca267
ramadhanina <ninshanni@gmail.com>
2021-05-22 16:42:06 +0700

    artikel

--- app/Http/Controllers/homecontroller.php
+++ app/Http/Controllers/homecontroller.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class homecontroller extends Controller
 {
-    //
+    public function index(){
+        return view("home0183");
+    }
+
+    public function artikel(){
+        return view("artikel0183");
+    }
 }
+
